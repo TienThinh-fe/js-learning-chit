c9006428903214e2b84dea67228ee3857af37689
Nhung Nguyen QA <nhungntc1412@gmail.com>
2025-06-06 02:10:37 +1000

    Do exercise DOM Manipulating 2

--- js-dom-events/exercises/js-dom-manipulating/script.js
+++ js-dom-events/exercises/js-dom-manipulating/script.js
@@ -16,17 +16,15 @@ document.addEventListener("DOMContentLoaded", function () {
   const container = document.querySelector(".container");
 
   // TODO: Create the alertContainer element
-  const newAlertContainer = document.createElement("div");
+  const alertContainer = document.createElement("div");
 
   // TODO: Set the id attribute of alertContainer to 'alertContainer'
-  newAlertContainer.setAttribute("id", "alertContainer");
+  alertContainer.setAttribute("id", "alertContainer");
 
   // TODO: Append the alertContainer to the container element
-  container.append(newAlertContainer);
-
+  container.append(alertContainer);
   // Now get a reference to your newly created alertContainer
-  const alertContainer = document.getElementById("alertContainer");
-
+  const getAlertContainer = document.getElementById("alertContainer");
   // Let's set up event listeners for our buttons
   showAlertBtn.addEventListener("click", createAndShowAlert);
   hideAlertBtn.addEventListener("click", removeAlert);
@@ -40,12 +38,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // TODO: Use createElement to create a new div element for the alert
     // Create a new div element that will serve as our alert
-    const newAlertSuccess = document.createElement("div");
-
+    const alertElement = document.createElement("div");
     // TODO: Use setAttribute to add the 'alert' class to the alert element
     // Add the 'alert' class to the element
-    newAlertSuccess.setAttribute("class", "alert alert-success");
-
+    alertElement.setAttribute("class", "alert alert-success");
     // TODO: Use setAttribute to add a type class like 'alert-success', 'alert-warning', 'alert-danger', or 'alert-info'
     // Add a specific alert type class (you can choose from: alert-success, alert-warning, alert-danger, alert-info)
     // newAlertSuccess.setAttribute("class", "alert-success");
@@ -55,40 +51,35 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // TODO: Create the content div for the alert message and add it to the alert
     // Create a div for alert content
-    const newAlertContent = document.createElement("div");
-    newAlertContent.setAttribute("class", "alert-content");
-
+    const alertContentElement = document.createElement("div");
     // TODO: Use innerHTML to set the content of the alert including HTML tags
     // Set the HTML content with a title and message (using innerHTML allows you to include HTML tags)
-    newAlertContent.innerHTML = "<strong>Success! He is my boyfriend </strong>";
-
+    alertContentElement.innerHTML =
+      "<strong>This is alert for this content!</strong>";
     // TODO: Create a close button for the alert
     // Create a button to close the alert
-    const newButtonCloseAlert = document.createElement("button");
-
+    const closeBtn = document.createElement("button");
     // TODO: Use innerText to set the text of the close button (without HTML)
     // Set the text of the close button (using innerText ensures it's treated as plain text, not HTML)
-    newButtonCloseAlert.innerText = "X";
-
+    closeBtn.innerText = "X";
     // TODO: Add an event listener to the close button to remove the alert when clicked
     // Make the close button functional by adding an event listener
-    newButtonCloseAlert.addEventListener("click", function () {
-      newAlertSuccess.remove();
+    closeBtn.addEventListener("click", function () {
+      alertElement.remove();
     });
     // TODO: Use append to add both the content div and close button to the alert element
     // Add the content div and close button to the alert element
-    newAlertSuccess.append(newAlertContent, newButtonCloseAlert);
-
+    alertElement.append(alertContentElement, closeBtn);
     // TODO: Use append to add the complete alert to the alertContainer
+    alertContainer.append(alertElement);
     // Add the complete alert to the container in the DOM
-    newAlertContainer.append(newAlertSuccess);
   }
 
   // This function will remove the alert from the DOM
   function removeAlert() {
     // TODO: Find the alert element and use the remove method to remove it from the DOM
     // Find and remove the alert if it exists
-    const findAlert = document.querySelector(".alert");
-    findAlert.remove();
+    const removeAlertElement = document.querySelector(".alert");
+    removeAlertElement.remove();
   }
 });
