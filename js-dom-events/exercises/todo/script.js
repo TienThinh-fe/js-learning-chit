1b2584297639ace5df8283a1b46904280f7eb2fc
Nhung Nguyen QA <nhungntc1412@gmail.com>
2025-06-14 15:03:15 +1000

    Do exercise todo 3 css

--- js-dom-events/exercises/todo/script.js
+++ js-dom-events/exercises/todo/script.js
@@ -8,34 +8,51 @@ const prioritySelect = document.getElementById("priority");
 
 const formatDate = (dateString) => {
   if (!dateString) return "No due date";
+
   const dateObject = new Date(dateString);
   const day = dateObject.getDate();
   const month = dateObject.getMonth() + 1;
   const year = dateObject.getFullYear();
+
   return `${day}/${month}/${year}`;
 };
+
+window.addEventListener("load", () => {
+  jobNameInput.focus();
+});
 
 addBtn.addEventListener("click", function () {
   if (jobNameInput.value === "") {
     return;
   }
+
   const listItem = document.createElement("li");
+
   const jobName = jobNameInput.value;
-  const deleteBtn = document.createElement("button");
   const dueDateString = dateInput.value;
-  // const dueDateObject = new Date(dueDateString);
-  // const day = dueDateObject.getDate();
-  // const month = dueDateObject.getMonth() + 1;
-  // const year = dueDateObject.getFullYear();
-  // const dueDate = `${day}/${month}/${year}`;
+  const priority = prioritySelect.value;
+  listItem.innerText = `${jobName} - ${formatDate(
+    dueDateString
+  )} - ${priority} `;
+
+  if (priority === "high") {
+    listItem.setAttribute("class", "todo-item todo-high");
+  } else if (priority === "medium") {
+    listItem.setAttribute("class", "todo-item todo-medium");
+  } else listItem.setAttribute("class", "todo-item todo-low");
+
+  const deleteBtn = document.createElement("button");
   deleteBtn.innerText = "Delete";
-  listItem.innerText = `${jobName} - ${formatDate(dueDateString)}`;
   deleteBtn.addEventListener("click", function () {
     listItem.remove();
   });
 
   listItem.append(deleteBtn);
   list.append(listItem);
+
   jobNameInput.value = "";
-  // dateInput.value = "";
 });
+
+// input validation, empty, min length = 2,
+// sepecial charactor -> error message.
+// default value for 'dueDate' = currrent time.
