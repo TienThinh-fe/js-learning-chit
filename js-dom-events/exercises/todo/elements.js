963c0255f4700fd7f42560ded1651326a528d37f
Nhung Nguyen QA <nhungntc1412@gmail.com>
2025-06-30 02:58:54 +1000

    Handle edit Dialog

--- js-dom-events/exercises/todo/elements.js
+++ js-dom-events/exercises/todo/elements.js
@@ -1,18 +1,19 @@
-const jobNameInput = document.getElementById('jobName')
-const addBtn = document.getElementById('add')
-const list = document.getElementById('list')
-const dateInput = document.getElementById('dueDate')
-const prioritySelect = document.getElementById('priority')
-const errorMessageEle = document.getElementById('errorMessage')
-const confirmationDialog = document.getElementById('confirmationDialog')
-const cancelDeleteBtn = document.getElementById('cancelDeleteBtn')
-const confirmDeleteBtn = document.getElementById('confirmDeleteBtn')
-const editDialog = document.getElementById('editDialog')
-const cancelEditBtn = document.getElementById('cancelEditBtn')
+const jobNameInput = document.getElementById("jobName");
+const addBtn = document.getElementById("add");
+const list = document.getElementById("list");
+const dateInput = document.getElementById("dueDate");
+const prioritySelect = document.getElementById("priority");
+const errorMessageEle = document.getElementById("errorMessage");
+const confirmationDialog = document.getElementById("confirmationDialog");
+const cancelDeleteBtn = document.getElementById("cancelDeleteBtn");
+const confirmDeleteBtn = document.getElementById("confirmDeleteBtn");
+const editDialog = document.getElementById("editDialog");
+const cancelEditBtn = document.getElementById("cancelEditBtn");
+const saveEditBtn = document.getElementById("saveEditBtn");
 
-const jobNameEditInput = document.getElementById('jobNameEdit')
-const dateEditInput = document.getElementById('dueDateEdit')
-const priorityEditSelect = document.getElementById('priorityEdit')
+const jobNameEditInput = document.getElementById("jobNameEdit");
+const dateEditInput = document.getElementById("dueDateEdit");
+const priorityEditSelect = document.getElementById("priorityEdit");
 
 export {
   jobNameInput,
@@ -29,4 +30,5 @@ export {
   priorityEditSelect,
   editDialog,
   cancelEditBtn,
-}
+  saveEditBtn,
+};
